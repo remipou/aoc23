08fff1957d30fc9b18bf9593674c8e4364d889fb
Rémi Puig <remi.puig@gmail.com>
2022-12-01 22:29:01 +0100

    types

--- 1dec.ts
+++ 1dec.ts
@@ -2,20 +2,20 @@
 const fs = require("fs");
 const data: string = fs.readFileSync("./data/1dec.txt", "utf8");
 // const data: string = fs.readFileSync("./samples/1dec.txt", "utf8");
-const foods = data
+const foods: Array<Array<number>> = data
   .split("\n\n")
-  .map((f: String) => f.split("\n").map((e) => Number(e)));
+  .map((f: string) => f.split("\n").map((e) => Number(e)));
 
 console.time("part 1");
-const calories = foods.map((f: Array<number>) =>
+const calories: Array<number> = foods.map((f: Array<number>) =>
   f.reduce((a: number, b: number) => a + b, 0)
 );
-const max = Math.max(...calories);
+const max: number = Math.max(...calories);
 console.timeEnd("part 1");
 console.log(max);
 
 console.time("part 2");
-const total = calories
+const total: number = calories
   .sort((a: number, b: number) => b - a)
   .slice(0, 3)
   .reduce((a: number, b: number) => a + b, 0);
