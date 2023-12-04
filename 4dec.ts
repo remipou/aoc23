cb608248ec009edf2b4ac65ca398c0cf2714a28d
Rémi Puig <remi.puig@gmail.com>
2023-12-04 09:18:18 +0100

    day 4. part 2

--- 4dec.ts
+++ 4dec.ts
@@ -1,22 +1,33 @@
 import getData from "./data";
 
 const [_ts, _file, date, test] = process.argv;
-const data: Array<any> = getData(date, test)
+
+interface Game {
+  id: number;
+  winningCards: Array<string>;
+  myCards: Array<string>;
+  count: number;
+}
+
+const data: Array<Game> = getData(date, test)
   .split("\n")
-  .map((e) => e.replace(/^Card\s+\d+:(.*)$/, "$1"))
+  .map((e) => e.replace(/^Card\s+(\d+):(.*)$/, "$1|$2"))
   .map((e) =>
     e
       .split("|")
       .map((i) => i.trim())
       .map((e) => e.split(" ").filter((e) => e.length))
-  );
+  )
+  .map((e) => ({
+    id: parseInt(e[0][0], 0),
+    winningCards: e[1],
+    myCards: e[2],
+    count: 1,
+  }));
 
 console.time("part 1");
-const calculatePoints = (
-  winningCards: Array<string>,
-  myCards: Array<string>
-): number => {
-  const matches = myCards.filter((c) => winningCards.includes(c));
+const calculatePoints = (game: Game): number => {
+  const matches = game.myCards.filter((c) => game.winningCards.includes(c));
   if (matches.length === 0) return 0;
   let p = 1;
   for (let i = 1; i < matches.length; i++) {
@@ -25,12 +36,21 @@ const calculatePoints = (
   return p;
 };
 const part1 = data
-  .map((e) => calculatePoints(e[0], e[1]))
+  .map((g) => calculatePoints(g))
   .reduce((prev, curr) => prev + curr, 0);
 
 console.timeEnd("part 1");
 console.log(part1);
 
 console.time("part 2");
+for (const g in data) {
+  const game = data[g];
+  const wins = game.myCards.filter((c) => game.winningCards.includes(c)).length;
+  for (let i = game.id; i < game.id + wins; i++) {
+    data[i].count = data[i].count + game.count;
+  }
+}
+
 console.timeEnd("part 2");
-// console.log(part2);
+
+console.log(data.reduce((prev: number, curr: Game) => prev + curr.count, 0));
