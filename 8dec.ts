ce822b5be6bfc1c50e8ef3b0becac1a59aa703ef
Rémi Puig <remi.puig@gmail.com>
2023-12-08 18:19:04 +0100

    day 8 - part 2 [unsolved]

--- 8dec.ts
+++ 8dec.ts
@@ -11,7 +11,9 @@ type I = {
 };
 const map = new Map<P, I>();
 raw[1].split("\n").map((line: string) => {
-  const matches = /^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$/g.exec(line);
+  const matches = /^([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)$/g.exec(
+    line
+  );
   if (matches) {
     const [_, elt, left, right] = matches;
     map.set(elt, { left, right });
@@ -23,21 +25,50 @@ console.time("part 1");
 const getDirection = (index: number): string =>
   instructions[index % instructions.length];
 
-let position = "AAA";
+const runPart1 = () => {
+  let position = "AAA";
+  let step = 0;
+  while (position !== "ZZZ") {
+    const next = map.get(position);
+    if (getDirection(step) === "L") {
+      position = next!.left;
+    } else {
+      position = next!.right;
+    }
+    step++;
+  }
+
+  console.timeEnd("part 1");
+  console.log("part 1", step);
+};
+// runPart1();
+
+console.time("part 2");
+
+const positions: string[] = [...map]
+  .filter(([k, v]) => k[2] === "A")
+  .map((e) => e[0]);
 let step = 0;
-while (position !== "ZZZ") {
-  const next = map.get(position);
+
+const allPositionsOk = () => positions.every((e) => e[2] === "Z");
+
+const move = (index: number): void => {
+  const next = map.get(positions[index]);
+  let position: string = "";
   if (getDirection(step) === "L") {
     position = next!.left;
   } else {
     position = next!.right;
   }
+  positions[index] = position;
+};
+
+while (!allPositionsOk()) {
+  for (let i = 0; i < positions.length; i++) {
+    move(i);
+  }
   step++;
 }
 
-console.timeEnd("part 1");
-console.log("part 1", step);
-
-console.time("part 2");
 console.timeEnd("part 2");
-// console.log(part2);
+console.log("part2", step);
