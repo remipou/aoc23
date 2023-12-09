b6de7f023df96dbd1d697373a5a82d4d70f4e23d
Rémi Puig <remi.puig@gmail.com>
2023-12-09 19:05:01 +0100

    day 8. part 2

--- 8dec.ts
+++ 8dec.ts
@@ -43,75 +43,45 @@ const runPart1 = () => {
 };
 // runPart1();
 
-console.time("part 2");
+console.time("part 2"); // from https://topaz.github.io/paste/#XQAAAQDvAwAAAAAAAAAyngpEyqrozfNdQRbrzFE3RH6xB9G5bS3TJqaPjFLD0lQQF9p/r5TBc28zT3NatwnWoOPTxqjGP48VrlMVLx/uwxBDcbNQ0yltqymAELLWzlPwyOesWiTawgiG158Lue4YxNraPZgYyqVmAh6VE1MY+8yozdTqqxtbelprkOQpI4Ke3cZoGe/w3NQ8Dz+BKyEwG8ms4UxkypmRxHemOT//rtPGZZe+T4QEBk9vTcnXKnaZsF3WQvSJ/qGdJwSWyxmAebD0C40s9laT2NBawjLxaMVhJNvGMDH8H0G7ZSKMD78/P5jxc1hMJPYZcU2cbRcoRqrYDrsTMvilcPmmsigBfF1k7LY0QTUcbd8vLc7wGgp/CZm9E3XSZWKp+bU0kSwULRCKxtOeFmCt4Nn/ykhZRCGquAmSZatzXKLUCKTCbdbPXfgnXPyf8o0DfbJsobMgeAhsJDgyCDFC0UChj489QSteRKcS9a49m+STA5h9WkzueOwXDO98GCg4Eyrp8FNHqDGZlAOXjmuvsD5J5QItQFRtQOarwBQ3SO//ZJBpo4VrFrsJ4PJ0O7AZKQ72MsCwEQ5B5471U3cocuZXI/8VjLK/f1Q55ea0iNvOvjrfjg7dM3xmUkAutUruSe59p0/tQgLb8sFG4bE0d1lwQPxXX9eRgEhvrSB7H6YdbalfwvZ6v1c09nyf3Bn19md6TP+vmPt+
 
-const positions: string[] = [...map]
-  .filter(([k, v]) => k[2] === "A")
-  .map((e) => e[0]);
-let step = 0;
+const part2 = (input: string): number => {
+  const lines = input.split("\n");
+  const nodes: Record<string, { L: string; R: string }> = {};
 
-const allPositionsOk = () => positions.every((e) => e[2] === "Z");
-
-const allSteps: number[] = [];
-
-function lcm(a: number, b: number): number {
-  return (a / gcd(a, b)) * b;
-}
-
-function gcd(a: number, b: number): number {
-  var t = 0;
-  a < b && ((t = b), (b = a), (a = t));
-  t = a % b;
-  return t ? gcd(b, t) : b;
-}
-
-// const lcm = (arr: number[]) => {
-//   // Return the Smallest Common Multiple for pair of Input Numbers.
-
-//   const [min, max] = arr.sort((a, b) => a - b);
-//   // Sorting Input Array to give min and max values for the range
-
-//   const range = Array(max - min + 1)
-//     .fill(0)
-//     .map((_, i) => i + min);
-//   // Initialize the Array
-
-//   let prodAll = range.reduce((product, number) => product * number);
-//   // Finding the product of all numbers which is a gauranteed multiple of the range
-
-//   let res = Array(prodAll / max)
-//     .fill(0)
-//     .map((_, i) => (i + 1) * max) // Initialize an Array of all the multiples of the 'max' number upto 'prodAll'
-//     .find((mulp) => range.every((val) => mulp % val === 0)); // check if any number meets the divisiblable criteria sooner then 'prodAll'
-
-//   return res;
-// };
-
-const move = (index: number): void => {
-  const next = map.get(positions[index]);
-  let position: string = "";
-  if (getDirection(step) === "L") {
-    position = next!.left;
-  } else {
-    position = next!.right;
+  for (let i = 1; i < lines.length; i++) {
+    const line = lines[i];
+    nodes[line.substring(0, 3)] = {
+      L: line.substring(7, 10),
+      R: line.substring(12, 15),
+    };
   }
-  positions[index] = position;
+
+  const instructions = lines[0].split("") as Array<"R" | "L">;
+  const starts: string[] = [];
+  for (const key in nodes) {
+    if (Object.prototype.hasOwnProperty.call(nodes, key) && key[2] === "A") {
+      starts.push(key);
+    }
+  }
+
+  const lengths = starts.map((start) => {
+    let steps = 0;
+    let curr = start;
+    for (let i = 0; curr[2] !== "Z"; i = (i + 1) % instructions.length) {
+      steps++;
+      curr = nodes[curr][instructions[i]];
+    }
+    return steps;
+  });
+
+  const gcd = (a: number, b: number) => {
+    while (b > 0) [a, b] = [b, a % b];
+    return a;
+  };
+  const lcm = (a: number, b: number) => (a * b) / gcd(a, b);
+  return lengths.reduce((n, x) => lcm(x, n), 1);
 };
 
-while (!allPositionsOk()) {
-  for (let i = 0; i < positions.length; i++) {
-    if (positions[i][2] !== "Z") {
-      move(i);
-    }
-    if (positions[i][2] === "Z" /*&& !allSteps.includes(step + 1)*/) {
-      allSteps.push(step + 1);
-    }
-  }
-  step++;
-}
-
 console.timeEnd("part 2");
-console.log(
-  "part2",
-  allSteps.reduce((acc, cur) => lcm(acc, cur), 1)
-);
+console.log("part2", part2(getData(date, test)));
