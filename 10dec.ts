a925ac6da8e3996394e6bc2e48835fe063af12d2
Rémi Puig <remi.puig@gmail.com>
2023-12-11 20:41:28 +0100

    day 10. part 1

--- 10dec.ts
+++ 10dec.ts
@@ -8,7 +8,6 @@ const data: Array<Array<string>> = getData(date, test)
 type MOVE = "UP" | "DOWN" | "LEFT" | "RIGHT";
 type POS = [number, number];
 
-console.log(data);
 let start: POS = [0, 0];
 for (let i = 0; i < data.length; i++) {
   if (data[i].includes("S")) {
@@ -16,52 +15,89 @@ for (let i = 0; i < data.length; i++) {
   }
 }
 
-/*****
-| 
--
-L
-J
-7
-F
-*******/
+console.time("part 1");
 
 const possibleMoves = (start: POS): MOVE[] => {
   const moves: MOVE[] = [];
   const [x, y] = start;
-  const upp = data[y - 1][x];
-  const down = data[y + 1][x];
-  const left = data[y][x - 1];
-  const right = data[y][x + 1];
-  console.log(start, upp, right, down, left);
-  switch (upp) {
-    case "|":
-    case "F":
-    case "7":
-      moves.push("UP");
-      break;
+  const current = data[y][x];
+  const up = y > 0 ? data[y - 1][x] : "nope";
+  const down = y < data.length - 1 ? data[y + 1][x] : "nope";
+  const left = x > 0 ? data[y][x - 1] : "nope";
+  const right = x < data[0].length - 1 ? data[y][x + 1] : "nope";
+  if (
+    (current === "S" || current === "-") &&
+    (right === "-" || right === "7" || right === "J")
+  ) {
+    moves.push("RIGHT");
   }
-  switch (down) {
-    case "|":
-    case "L":
-    case "J":
-      moves.push("DOWN");
-      break;
+  if (
+    (current === "S" || current === "-") &&
+    (left === "-" || left === "L" || left === "F")
+  ) {
+    moves.push("LEFT");
   }
-  switch (left) {
-    case "-":
-    case "L":
-    case "F":
-      moves.push("LEFT");
-      break;
+  if (
+    (current === "S" || current === "|") &&
+    (up === "7" || up === "F" || up === "|")
+  ) {
+    moves.push("UP");
   }
-  switch (right) {
-    case "-":
-    case "J":
-    case "7":
-      moves.push("RIGHT");
-      break;
+  if (
+    (current === "S" || current === "|") &&
+    (down === "L" || down === "J" || down === "|")
+  ) {
+    moves.push("DOWN");
   }
-  return moves;
+  if (
+    (current === "S" || current === "L") &&
+    (right === "-" || right === "7" || right === "J")
+  ) {
+    moves.push("RIGHT");
+  }
+  if (
+    (current === "S" || current === "L") &&
+    (up === "|" || up === "7" || up === "F")
+  ) {
+    moves.push("UP");
+  }
+  if (
+    (current === "S" || current === "7") &&
+    (left === "-" || left === "L" || left === "F")
+  ) {
+    moves.push("LEFT");
+  }
+  if (
+    (current === "S" || current === "7") &&
+    (down === "|" || down === "J" || down === "L")
+  ) {
+    moves.push("DOWN");
+  }
+  if (
+    (current === "S" || current === "J") &&
+    (left === "-" || left === "L" || left === "F")
+  ) {
+    moves.push("LEFT");
+  }
+  if (
+    (current === "S" || current === "J") &&
+    (up === "|" || up === "F" || up === "7")
+  ) {
+    moves.push("UP");
+  }
+  if (
+    (current === "S" || current === "F") &&
+    (right === "-" || right === "7" || right === "J")
+  ) {
+    moves.push("RIGHT");
+  }
+  if (
+    (current === "S" || current === "F") &&
+    (down === "|" || down === "L" || down === "J")
+  ) {
+    moves.push("DOWN");
+  }
+  return [...new Set(moves)];
 };
 
 const makeMove = (pos: POS, move: MOVE): POS => {
@@ -78,37 +114,53 @@ const makeMove = (pos: POS, move: MOVE): POS => {
 };
 
 const makeStep = (pos: POS, last: POS | null = null): POS => {
-  const moves = possibleMoves(start);
+  const moves = possibleMoves(pos);
   let move: MOVE;
   if (last === null) {
     move = moves[1];
+  } else if (last[0] === start[0] && last[1] === start[1]) {
+    move = moves[0];
   } else {
-    const testMove0 = makeMove(pos, moves[0]);
-    if (testMove0[0] !== last[0] && testMove0[1] !== last[1]) {
-      move = moves[0];
-    } else {
+    const testMove = makeMove(pos, moves[0]);
+    if (testMove[0] === last[0] && testMove[1] === last[1]) {
       move = moves[1];
+    } else {
+      move = moves[0];
     }
   }
-  console.log("===>", move);
+
+  // console.log(pos, "===>", data[pos[1]][pos[0]], "===>", moves, "===>", move);
   return makeMove(pos, move);
 };
-console.log("start --->", start);
+console.log("start", start, data[start[1]][start[0]]);
 let position: POS = makeStep(start);
 let step: number = 1;
 let last: POS = [...start];
-while (position[0] !== start[0] || position[1] !== start[1]) {
-  console.log("position", position, data[position[1]][position[0]]);
+const distances = new Map<number, number>();
+do {
   const copyLast: POS = [...last];
   last = [...position];
   position = makeStep(position, copyLast);
+  if (position) {
+    distances.set(
+      step,
+      Math.abs(position[0] - start[0]) + Math.abs(position[1] - start[1])
+    );
+  }
   step++;
-}
+} while (position);
 
-console.time("part 1");
+// const getAllIndexes = (arr: number[], val: number) => {
+//   const indexes: number[] = [];
+//   let i: number = -1;
+//   while ((i = arr.indexOf(val, i + 1)) != -1) {
+//     indexes.push(i);
+//   }
+//   return indexes;
+// };
 
 console.timeEnd("part 1");
-// console.log(part1);
+console.log("part 1", step / 2);
 
 console.time("part 2");
 console.timeEnd("part 2");
