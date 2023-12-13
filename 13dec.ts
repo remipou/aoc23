6882923d334c1a690f5f485f810994a1079e30f6
Rémi Puig <remi.puig@gmail.com>
2023-12-13 21:41:31 +0100

    part 2. nearly working

--- 13dec.ts
+++ 13dec.ts
@@ -67,5 +67,64 @@ console.log(
 );
 
 console.time("part 2");
+
+const deepCopy = (grid: Grid): Grid =>
+  grid.map(function (row: Row) {
+    return row.slice();
+  });
+
+const processPart2 = (grid: Grid): number => {
+  const originalHorizontalReflection = findReflection(grid);
+  const originalVerticalReflection = findReflection(rotate(grid));
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const newGrid = deepCopy(grid);
+      const elt = grid[i][j];
+      newGrid[i][j] = elt === "#" ? "." : "#";
+      const horizontalReflection = findReflection(newGrid);
+      if (
+        horizontalReflection > 0 &&
+        horizontalReflection !== originalHorizontalReflection
+      ) {
+        console.log(
+          "smudge at (horizontal reflection)",
+          { x: j, y: i },
+          horizontalReflection
+        );
+        return horizontalReflection * 100;
+      }
+    }
+  }
+  console.log("continuing");
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      const newGrid = deepCopy(grid);
+      const elt = grid[i][j];
+      newGrid[i][j] = elt === "#" ? "." : "#";
+      const verticalReflection = findReflection(rotate(newGrid));
+      if (
+        verticalReflection > 0 &&
+        verticalReflection !== originalVerticalReflection
+      ) {
+        console.log(
+          "smudge at (vertical reflection)",
+          { x: j, y: i },
+          verticalReflection
+        );
+        return verticalReflection;
+      }
+    }
+  }
+  console.log("no reflection");
+  return 0;
+};
+
 console.timeEnd("part 2");
-// console.log(part2);
+console.log(data.map((g) => processPart2(g as Grid)));
+console.log(
+  "part2",
+  data
+    .map((g) => processPart2(g as Grid))
+    .reduce((prev, curr) => prev + curr, 0)
+);
