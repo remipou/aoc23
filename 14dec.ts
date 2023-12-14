54638bad944317a4fd4411d11132916b82314337
Rémi Puig <remi.puig@gmail.com>
2023-12-14 10:03:36 +0100

    fix bugs in slide functions

--- 14dec.ts
+++ 14dec.ts
@@ -8,12 +8,14 @@ const data: T = getData(date, test)
   .split("\n")
   .map((l) => l.split(""));
 
-const visualize = (d: T): void =>
-  console.log(d.map((r) => r.join("")).join("\n") + "\n");
+const stringify = (d: T): string => d.map((r) => r.join("")).join("\n") + "\n";
+const visualize = (d: T): void => console.log(stringify(d));
+const deepCopy = (d: T): T => d.map((row: string[]) => row.slice());
 
 console.time("part 1");
 
-const slideNorth = (d: T): T => {
+const slideNorth = (d0: T): T => {
+  const d = deepCopy(d0);
   for (let row = 0; row < d.length; row++) {
     for (let col = 0; col < d[row].length; col++) {
       if (d[row][col] === "O") {
@@ -29,12 +31,13 @@ const slideNorth = (d: T): T => {
   return d;
 };
 
-const slideSouth = (d: T): T => {
+const slideSouth = (d0: T): T => {
+  const d = deepCopy(d0);
   for (let row = d.length - 1; row >= 0; row--) {
     for (let col = 0; col < d[row].length; col++) {
       if (d[row][col] === "O") {
         let newRow = row;
-        while (newRow < d[row].length - 1 && d[newRow + 1][col] === ".") {
+        while (newRow < d.length - 1 && d[newRow + 1][col] === ".") {
           d[newRow + 1][col] = "O";
           d[newRow][col] = ".";
           newRow++;
@@ -45,14 +48,15 @@ const slideSouth = (d: T): T => {
   return d;
 };
 
-const slideEast = (d: T): T => {
+const slideEast = (d0: T): T => {
+  const d = deepCopy(d0);
   for (let row = 0; row < d.length; row++) {
     for (let col = d[row].length - 1; col >= 0; col--) {
       if (d[row][col] === "O") {
         let newCol = col;
         while (newCol < d[row].length - 1 && d[row][newCol + 1] === ".") {
           d[row][newCol + 1] = "O";
-          d[row][col] = ".";
+          d[row][newCol] = ".";
           newCol++;
         }
       }
@@ -61,14 +65,15 @@ const slideEast = (d: T): T => {
   return d;
 };
 
-const slideWest = (d: T): T => {
+const slideWest = (d0: T): T => {
+  const d = deepCopy(d0);
   for (let row = 0; row < d.length; row++) {
     for (let col = 0; col < d[0].length; col++) {
       if (d[row][col] === "O") {
         let newCol = col;
-        while (newCol > 0 && d[row][col - 1] === ".") {
+        while (newCol > 0 && d[row][newCol - 1] === ".") {
           d[row][newCol - 1] = "O";
-          d[row][col] = ".";
+          d[row][newCol] = ".";
           newCol--;
         }
       }
@@ -90,17 +95,22 @@ console.log("part1", getLoad(slideNorth(data)));
 console.time("part 2");
 
 const cycle = (d: T): T => {
-  slideNorth(d);
-  slideWest(d);
-  slideSouth(d);
-  slideEast(d);
-  return d;
+  let d1 = deepCopy(d);
+  d1 = slideNorth(d1);
+  d1 = slideWest(d1);
+  d1 = slideSouth(d1);
+  d1 = slideEast(d1);
+  return d1;
 };
+const cycles = new Map<string, number>();
 
-for (let i = 0; i < 3; i++) {
-  cycle(data);
-  visualize(data);
+let d: T = deepCopy(data);
+for (let i = 0; i < 4; i++) {
+  visualize(d);
+  d = cycle(d);
 }
 
+// console.log(cycles);
+
 console.timeEnd("part 2");
-console.log("part2", getLoad(slideNorth(data)));
+// console.log("part2", getLoad(endState));
