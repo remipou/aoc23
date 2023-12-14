c52904b4441586e0ee5537a5ca6e167d2a2d83ca
Rémi Puig <remi.puig@gmail.com>
2023-12-14 08:33:18 +0100

    day 14. part 2. wip

--- 14dec.ts
+++ 14dec.ts
@@ -8,28 +8,73 @@ const data: T = getData(date, test)
   .split("\n")
   .map((l) => l.split(""));
 
-const deepCopy = (a: T): T => a.map((b: string[]) => b.slice());
-
 const visualize = (d: T): void =>
   console.log(d.map((r) => r.join("")).join("\n") + "\n");
 
 console.time("part 1");
 
 const slideNorth = (d: T): T => {
-  const d2 = deepCopy(d);
-  for (let row = 0; row < d2.length; row++) {
-    for (let col = 0; col < d2[row].length; col++) {
-      if (d2[row][col] === "O") {
+  for (let row = 0; row < d.length; row++) {
+    for (let col = 0; col < d[row].length; col++) {
+      if (d[row][col] === "O") {
         let newRow = row;
-        while (newRow > 0 && d2[newRow - 1][col] === ".") {
-          d2[newRow - 1][col] = "O";
-          d2[newRow][col] = ".";
+        while (newRow > 0 && d[newRow - 1][col] === ".") {
+          d[newRow - 1][col] = "O";
+          d[newRow][col] = ".";
           newRow--;
         }
       }
     }
   }
-  return d2;
+  return d;
+};
+
+const slideSouth = (d: T): T => {
+  for (let row = d.length - 1; row >= 0; row--) {
+    for (let col = 0; col < d[row].length; col++) {
+      if (d[row][col] === "O") {
+        let newRow = row;
+        while (newRow < d[row].length - 1 && d[newRow + 1][col] === ".") {
+          d[newRow + 1][col] = "O";
+          d[newRow][col] = ".";
+          newRow++;
+        }
+      }
+    }
+  }
+  return d;
+};
+
+const slideEast = (d: T): T => {
+  for (let row = 0; row < d.length; row++) {
+    for (let col = d[row].length - 1; col >= 0; col--) {
+      if (d[row][col] === "O") {
+        let newCol = col;
+        while (newCol < d[row].length - 1 && d[row][newCol + 1] === ".") {
+          d[row][newCol + 1] = "O";
+          d[row][col] = ".";
+          newCol++;
+        }
+      }
+    }
+  }
+  return d;
+};
+
+const slideWest = (d: T): T => {
+  for (let row = 0; row < d.length; row++) {
+    for (let col = 0; col < d[0].length; col++) {
+      if (d[row][col] === "O") {
+        let newCol = col;
+        while (newCol > 0 && d[row][col - 1] === ".") {
+          d[row][newCol - 1] = "O";
+          d[row][col] = ".";
+          newCol--;
+        }
+      }
+    }
+  }
+  return d;
 };
 
 const getLoad = (d: T) =>
@@ -40,9 +85,22 @@ const getLoad = (d: T) =>
     .reduce((prev, curr) => prev + curr, 0);
 
 console.timeEnd("part 1");
-
 console.log("part1", getLoad(slideNorth(data)));
 
 console.time("part 2");
+
+const cycle = (d: T): T => {
+  slideNorth(d);
+  slideWest(d);
+  slideSouth(d);
+  slideEast(d);
+  return d;
+};
+
+for (let i = 0; i < 3; i++) {
+  cycle(data);
+  visualize(data);
+}
+
 console.timeEnd("part 2");
-// console.log(part2);
+console.log("part2", getLoad(slideNorth(data)));
